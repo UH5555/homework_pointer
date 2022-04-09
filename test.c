08734d8f9d3e90e4c1232564299e4dfa31174077
UH5555 <zhuhao162@163.com>
2022-04-09 11:27:22 +0800

    0409\指针作业（3）

--- test.c
+++ test.c
@@ -139,46 +139,154 @@
 //	return 0;
 //}
 
-void print(int arr[], int sz)
-{
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-}
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//
+//int move(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left<right)
+//	{
+//		//从左边找偶数
+//		while ((left<right) && (arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		//从右边找奇数
+//		while ((left < right) && (arr[right] % 2 == 0))
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	move(arr,sz);
+//	print(arr, sz);
+//	return 0;
+//}
 
-int move(int arr[], int sz)
-{
-	int left = 0;
-	int right = sz - 1;
-	while (left<right)
-	{
-		//从左边找偶数
-		while ((left<right) && (arr[left] % 2 == 1))
-		{
-			left++;
-		}
-		//从右边找奇数
-		while ((left < right) && (arr[right] % 2 == 0))
-		{
-			right--;
-		}
-		if (left < right)
-		{
-			int tmp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = tmp;
-		}
-	}
+//int main()
+//{
+//	unsigned char a = 200;
+//	unsigned char b = 100;
+//	unsigned char c = 0;
+//
+//	c = a + b;//整型提升
+//	//00000000000000000000000011001000 - a
+//	//00000000000000000000000001100100 - b
+//	//00000000000000000000000100101100
+//	//00101100 - c
+//
+//	//00000000000000000000000011001000
+//	//11001000 - a
+//	//00000000000000000000000001100100
+//	//01100100 - b
+//	printf("%d %d", a + b, c);
+//	//a+b - 00000000000000000000000100101100 - 补码
+//	//c 00101100
+//	return 0;
+//}
 
-}
+//int main()
+//{
+//	unsigned int a = 0x1234;
+//	unsigned char b = *(unsigned char*)&a;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//大端存储模式下，b为0x00；
 
-int main()
-{
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	move(arr,sz);
-	print(arr, sz);
-	return 0;
-}
+//#include <string.h>
+//int main()
+//{
+//	//-128 --->  127
+//	char a[1000] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	//-1 -2 ... -128 127 126 125 ... 3 2 1 0 -1 
+//	printf("%d\n", strlen(a));
+//	//255
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j < 10; j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i == j)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i >= 2 && j >= 1)
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//		}
+//	}
+//	//打印
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <=i; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//A说：不是我
+//B说：是C
+//C说：是D
+//D说：C在胡说
+//已知三个人说了真话，一个人说了假话。
+//现在根据这些信息，写一个程序来确定谁是凶手。
+//
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("killer =%c\n", killer);
+//		}
+//	}
+//	return 0;
+//}
+
+//赛马问题：有36匹马，6个跑道，没有计时器，请赛马确定，36匹马中的前三名。
+//请问最少比赛几次？8次
+
+//烧香问题
+//有一种香，材质不均匀，但是每一根这样香，燃完恰好是一个小时。
+//给你2根香，帮我确定一个15分钟的时间段。
